232e26df82a9a38590701ce20858f3a8d3aca2f1
chrispetito <christopherpetito@gmail.com>
2019-05-15 16:17:16 -0700

    add nav bar, styling to body and character cards

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,7 +3,7 @@ import React from "react";
 const Character = props => {
   return (
     <div className='character-card'>
-      <h1>{props.character.name}</h1>
+      <h1 className='character-name'>{props.character.name}</h1>
       <h3>Height: {props.character.height}</h3>
       <h3>Hair Color: {props.character.hair_color}</h3>
       <h3>Gender: {props.character.gender}</h3>
